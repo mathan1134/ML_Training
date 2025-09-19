2b2f0798ed47ecdff0c96b3330da3f2be1fc90c6
mathan1134 <mathanjose768@gmail.com>
2025-09-19 11:44:46 +0530

    Initial commit

--- deeplearn/nn.py
+++ deeplearn/nn.py
@@ -14,6 +14,42 @@ X = np.array([[0,0],
 y = np.array([[0],[1],[1],[0]])
 
 np.random.seed(42)
-input_layer_neurons = 2
-hidden_layer_neurons = 2
-output_neurons = 1
+iln=2
+hln=2
+on=1
+
+w1=np.random.uniform(size=(iln,hln))
+b1=np.random.uniform(size=(1,hln))
+w2=np.random.uniform(size=(hln,on))
+b2=np.random.uniform(size=(1,on))
+
+epochs=8000
+learning_rate=0.3
+
+
+
+# training
+for _ in range(epochs):
+    hidden_input=np.dot(X,w1)+b1
+    hidden_op=sigmoid(hidden_input)
+
+    final_input=np.dot(hidden_op,w2)+b2
+    final_op=sigmoid(final_input)
+
+    # backbrobagation
+    error=y-final_op
+    d_op=error*sigmoid_derivative(final_op)
+
+    err_hid=d_op.dot(w2.T)
+    d_hidd=err_hid*sigmoid_derivative(hidden_op)
+
+
+    # update bias&weights
+    w2 += hidden_op.T.dot(d_op) * learning_rate
+    b2 += np.sum(d_op, axis=0, keepdims=True) * learning_rate
+    w1 += X.T.dot(d_hidd) * learning_rate
+    b1 += np.sum(d_hidd, axis=0, keepdims=True) * learning_rate
+
+# Final output after training
+print("Predictions after training:")
+print(final_op)
