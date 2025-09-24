c9389ad07ec9ff93e3c5716b49c8b3b568b2a270
mathan1134 <mathanjose768@gmail.com>
2025-09-24 13:25:33 +0530

    Initial commit

--- deeplearn/frame.py
+++ deeplearn/frame.py
@@ -36,3 +36,36 @@ import torch.optim as optim
 X=torch.tensor([[1,0],[0,1]],dtype=torch.float32)
 y=torch.tensor([[1],[0]],dtype=torch.float32)
 
+class simpleNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.hidden=nn.Linear(2,5)
+        self.output=nn.Linear(5,1)
+        self.sigmoid=nn.Sigmoid()
+        self.relu=nn.ReLU()
+
+    def forward(self,x)    :
+        x=self.relu(self.hidden(x))
+        x=self.sigmoid(self.output(x))
+        return x
+    
+model=simpleNN()    
+criterion=nn.BCELoss()
+optimizer=optim.Adam(model.parameters(),lr=0.01)
+
+for epoch in range(500):
+    optimizer.zero_grad()
+    output=model(X)
+    loss=criterion(output,y)    # loss funct
+    loss.backward()             #backprobagation
+    optimizer.step()            #update weights
+
+
+with torch.no_grad():
+    prediction=model(X)
+    for i,p in enumerate(prediction):
+        print(f"input{X[i].numpy()}---> prediction{p.item():.3f}")
+
+
+
+
